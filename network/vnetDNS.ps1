6233d12e85ff4f3b91041e1804fc51d9f545f6fb
ravi mishra <42548254+hanuravim@users.noreply.github.com>
2019-02-27 04:06:59 -0600

    Update vnetDNS.ps1

--- network/vnetDNS.ps1
+++ network/vnetDNS.ps1
@@ -1,13 +1,13 @@
 $applicationId = Get-AutomationVariable -Name 'applicationId'
 $tenantId = Get-AutomationVariable -Name 'tenantId'
-$secureStringPwd = Get-AutomationVariable -Name 'secureStringPwd'
+$appsecret = Get-AutomationVariable -Name 'secureStringPwd'
 $virtualNetworkResourceGroup = Get-AutomationVariable -Name ''
 $virtualnetworkname = Get-AutomationVariable -Name 'virtualNetworkResourceGroup'
 $pdc_networkInterfaceIP = Get-AutomationVariable -Name 'pdc_networkInterfaceIP'
 
 
 #LOGON TO AZURE
-$secpasswd = ConvertTo-SecureString $secureStringPwd -AsPlainText -Force
+$secpasswd = ConvertTo-SecureString $appsecret -AsPlainText -Force
 ($creds = New-Object System.Management.Automation.PSCredential ($applicationId, $secpasswd))
 Connect-AzureRmAccount -ServicePrincipal -Credential $credential -TenantId $tenantId
 
