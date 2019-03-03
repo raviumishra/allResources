131a08c3010731d5693de05cea3022e52704040d
ravi mishra <42548254+hanuravim@users.noreply.github.com>
2019-03-03 15:49:23 -0600

    Update setVNET_DNS.ps1

--- network/setVNET_DNS.ps1
+++ network/setVNET_DNS.ps1
@@ -11,8 +11,8 @@ Set-MpPreference -DisableRealtimeMonitoring $true
 
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-Install-Module Azure -Confirm:$False
-Import-Module Azure
+Install-Module -Name AzureRM -RequiredVersion 6.9.0 -Confirm:$False
+Import-Module AzureRM
 
 $secpasswd = ConvertTo-SecureString $appsecret -AsPlainText -Force
 ($creds = New-Object System.Management.Automation.PSCredential ($applicationId, $secpasswd))
