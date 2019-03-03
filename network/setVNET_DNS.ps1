b17635595219010a169e6b9920b21e6de69a854b
ravi mishra <42548254+hanuravim@users.noreply.github.com>
2019-03-03 04:31:55 -0600

    Update setVNET_DNS.ps1

--- network/setVNET_DNS.ps1
+++ network/setVNET_DNS.ps1
@@ -1,4 +1,8 @@
-Install-Module -Name AzureRM.Automation -Confirm:$False
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+Install-Module Azure -Confirm:$False
+Import-Module Azure
+
 $appsecret = 'rkULdSvegHmypJv0hc6HDLy4hhAn3XgU+qQCvGukPM4='
 $applicationId = '272de8a0-d263-4a98-a79f-ca58dcdef45c'
 $tenantId = 'e525031f-bb1f-4659-aa0e-c0f3fbfa832f'
