e54c382b581a7e8ecebdc7ff95463479b53ebdaa
ravi mishra <42548254+hanuravim@users.noreply.github.com>
2019-03-03 13:30:43 -0600

    Update setVNET_DNS.ps1

--- network/setVNET_DNS.ps1
+++ network/setVNET_DNS.ps1
@@ -1,12 +1,21 @@
-Install-Module AzureRM -AllowClobber
-Import-Module AzureRM
+Param (
+[Parameter()]
+[String]$appsecret = 'rkULdSvegHmypJv0hc6HDLy4hhAn3XgU+qQCvGukPM4=',
+[String]$applicationId = '272de8a0-d263-4a98-a79f-ca58dcdef45c',
+[String]$tenantId = 'e525031f-bb1f-4659-aa0e-c0f3fbfa832f',
+[String]$automationAccountResourceGroup = 'GAV-EMS-DEV-TEN-SCU-02',
+[String]$automationAccount = 'GAV-EMS-DEV-TEN-SCU-04'
+)
+#DISABLE WINDOWS DEFENDER
+Set-MpPreference -DisableRealtimeMonitoring $true
 
-$appsecret = 'rkULdSvegHmypJv0hc6HDLy4hhAn3XgU+qQCvGukPM4='
-$applicationId = '272de8a0-d263-4a98-a79f-ca58dcdef45c'
-$tenantId = 'e525031f-bb1f-4659-aa0e-c0f3fbfa832f'
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+Install-Module Azure -Confirm:$False
+Import-Module Azure
 
 $secpasswd = ConvertTo-SecureString $appsecret -AsPlainText -Force
 ($creds = New-Object System.Management.Automation.PSCredential ($applicationId, $secpasswd))
 Connect-AzureRmAccount -ServicePrincipal -Credential $creds -TenantId $tenantId
 
-Start-AzureRmAutomationRunbook -Name vnetDNS_runbook -ResourceGroupName GAV-EMS-DEV-TEN-SCU-02 -AutomationAccountName GAV-EMS-DEV-TEN-SCU-04
+Start-AzureRmAutomationRunbook -Name vnetDNS_runbook -ResourceGroupName $automationAccountResourceGroup  -AutomationAccountName $automationAccount 
