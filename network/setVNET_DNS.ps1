2236936e02f5b9e7f9bd2554ab84ce792c63dec2
ravi mishra <42548254+hanuravim@users.noreply.github.com>
2019-03-03 02:17:07 -0600

    Update setVNET_DNS.ps1

--- network/setVNET_DNS.ps1
+++ network/setVNET_DNS.ps1
@@ -1,4 +1,4 @@
-Install-Module -Name AzureRM.Automation
+Install-Module -Name AzureRM.Automation -Confirm:$False
 $appsecret = 'rkULdSvegHmypJv0hc6HDLy4hhAn3XgU+qQCvGukPM4='
 $applicationId = '272de8a0-d263-4a98-a79f-ca58dcdef45c'
 $tenantId = 'e525031f-bb1f-4659-aa0e-c0f3fbfa832f'
